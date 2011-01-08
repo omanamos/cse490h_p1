1056bd080d9fc36f1210aef42f876c9b3cd3963e
omanamos <omanamos@gmail.com>
2011-01-08 12:34:58 -0800

    Command Handling

--- proj/DistNode.java
+++ proj/DistNode.java
@@ -15,6 +15,7 @@
 				System.out.println("10 File does not exist");
 			}
  */
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import edu.washington.cs.cse490h.lib.PersistentStorageReader;
@@ -22,9 +23,43 @@ import edu.washington.cs.cse490h.lib.Utility;
 
 public class DistNode extends RIONode {
 
+	//TODO: Add Error reporting
 	@Override
 	public void onRIOReceive(Integer from, int protocol, byte[] msg) {
-		
+		String data = Utility.byteArrayToString(msg);
+		switch(protocol){
+		case Protocol.APPEND: case Protocol.PUT:
+			String[] parts = data.split(" ");
+			try {
+				this.getWriter(parts[0], protocol == Protocol.APPEND).write(parts[1]);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			break;
+		case Protocol.CREATE:
+			try {
+				this.getWriter(data, false);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			break;
+		case Protocol.DELETE:
+			try{
+				this.getWriter(data, false).delete();
+			}catch(IOException e){
+				e.printStackTrace();
+			}
+			break;
+		case Protocol.GET:
+			try {
+				printReader(this.getReader(data));
+			} catch (FileNotFoundException e) {
+				System.out.println("10 File does not exist");
+			}
+			break;
+		default:
+			return;
+		}
 	}
 
 	@Override
@@ -38,18 +73,39 @@ public class DistNode extends RIONode {
 		int server = Integer.parseInt(parts[1]);
 		
 		if(parts[0].equals("create")){
-			this.RIOLayer.RIOSend(server, Protocol.DATA, Utility.stringToByteArray(command));
+			this.create(server, parts[2]);
 		}else if(parts[0].equals("get")){
-			
+			this.get(server, parts[2]);
 		}else if(parts[0].equals("put")){
-			
+			this.put(server, parts[2], parts[3]);
 		}else if(parts[0].equals("append")){
-			
+			this.append(server, parts[2], parts[3]);
 		}else if(parts[0].equals("delete")){
-			
+			this.delete(server, parts[2]);
 		}else{
-			
+			System.out.println("Invalid command.");
 		}
+	}
+	
+	//TODO: check for max packet size
+	private void create(int server, String filename){
+		this.RIOLayer.RIOSend(server, Protocol.CREATE, Utility.stringToByteArray(filename));
+	}
+	
+	private void get(int server, String filename){
+		this.RIOLayer.RIOSend(server, Protocol.GET, Utility.stringToByteArray(filename));
+	}
+	
+	private void put(int server, String filename, String contents){
+		this.RIOLayer.RIOSend(server, Protocol.PUT, Utility.stringToByteArray(filename + " " + contents));
+	}
+	
+	private void append(int server, String filename, String contents){
+		this.RIOLayer.RIOSend(server, Protocol.APPEND, Utility.stringToByteArray(filename + " " + contents));
+	}
+	
+	private void delete(int server, String filename){
+		this.RIOLayer.RIOSend(server, Protocol.DELETE, Utility.stringToByteArray(filename));
 	}
 	
 	private void printReader(PersistentStorageReader r){
