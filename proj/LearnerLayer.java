1e4c5d56c7d868cd0719e85d48651890045975d6
Brad Bicknell <fxfilmxf@gmail.com>
2011-02-27 15:37:39 -0800

    more learner layer stuff

--- proj/LearnerLayer.java
+++ proj/LearnerLayer.java
@@ -2,8 +2,6 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.*;
 
-import javax.swing.text.Utilities;
-
 import edu.washington.cs.cse490h.lib.PersistentStorageReader;
 import edu.washington.cs.cse490h.lib.PersistentStorageWriter;
 import edu.washington.cs.cse490h.lib.Utility;
@@ -32,56 +30,10 @@ public class LearnerLayer {
 		this.readOutOfOrder();
 	}
 	
-	private void readLog() {
-		try {
-			PersistentStorageReader r = this.n.getReader(LEARN_FILE);
-			String line = r.readLine();
-			while( line != null ) {
-				String[] entry = line.split(" ");
-				int instanceNum = Integer.parseInt(entry[0]);
-				int proposalNum = Integer.parseInt(entry[1]);
-				learned.put( instanceNum, proposalNum );
-				updateLargestInstanceNum( instanceNum );
-			}
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
 	public void updateLargestInstanceNum( int instanceNum ) {
 		if( instanceNum > this.largestInstanceNum ) {
 			this.largestInstanceNum = instanceNum;
 		}
-	}
- 	
-	private void readOutOfOrder() {
-	try {
-		PersistentStorageReader r = this.n.getReader(OUT_OF_ORDER);
-		String line = r.readLine();
-		while( line != null ) {
-			String[] entry = line.split("|");
-			int instanceNum = Integer.parseInt(entry[0]);
-			int proposalNum = Integer.parseInt(entry[1]);
-			String value = entry[2];
-			Proposal p = new Proposal( instanceNum, proposalNum, value );
-			proposals.put( instanceNum, p);
-			updateLargestInstanceNum( instanceNum );
-		}
-	} catch (FileNotFoundException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	} catch (IOException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
-	}
-	
-	private void send(int dest, PaxosPacket pkt){
-		this.paxosLayer.send(dest, pkt);
 	}
 	
 	public void receive(int from, PaxosPacket pkt) {
@@ -95,7 +47,70 @@ public class LearnerLayer {
 	}
 	
 	private void receivedLearn(int from, PaxosPacket pkt){
-		learnProposal(pkt);
+		Proposal p = new Proposal(pkt);
+		executeProposal( p );
+	}
+	
+	
+	private void executeProposal( Proposal p ) {
+		int iNum = p.getInstanceNum();
+		
+		proposals.put(iNum, p ); //add this proposal to our proposals...
+		updateLargestInstanceNum(iNum);
+		
+		if( iNum == this.lastContInstance + 1 ) {
+			this.lastContInstance++;
+			//ok we can process this
+			this.pushProposalValueToDisk(p);
+			
+			//add to our learned
+			learned.put( iNum, p.getProposalNum() );
+			writeToLog( p );
+			
+			//now see if we can execute more proposals 
+			this.executeNextLearnedProposal();
+		} else if( iNum > this.lastContInstance + 1 ) {
+			//ok we are missing something so write this proposal to the out of order log
+			writeOutOfOrder( p );
+		}
+	}
+	
+	private void executeNextLearnedProposal() {
+		Proposal p = proposals.get(this.lastContInstance + 1); //the next higher instance
+		executeProposal( p );
+	}
+	
+	private void pushProposalValueToDisk( Proposal p ) {
+		if( canCommit( p ) ) {
+			//write the transaction to disk
+		} else {
+			//write an abort to the log
+			
+		}
+	}
+	
+	private boolean canCommit( Proposal p ) {
+		//call something on the transaction layer to see if we can commit
+		return true;
+	}
+	
+	
+	
+	/**
+	 * Leader methods
+	 * 
+	 * 
+	 * 
+	 */
+	
+	
+	private void sendChosenToAllLearners( Proposal p ) {
+		int[] learners = PaxosLayer.LEARNERS;
+		PaxosPacket learnPacket = new PaxosPacket(PaxosProtocol.LEARN, p.getProposalNum(), p.getInstanceNum(), Utility.stringToByteArray(p.getValue()));
+		for( int learner : learners ) {
+			if( learner != this.n.addr ) //do not send to ourself
+				this.send(learner, learnPacket);
+		}
 	}
 	
 	private void receivedAccept(int from, PaxosPacket pkt){
@@ -124,75 +139,53 @@ public class LearnerLayer {
 		
 		//if we have the majority then do stuff
 		if( pcount >= majority ) {
-			proposalCount.remove(pkt.getInstanceNumber()); //not sure if we should remove from our counts.
-			writeToLog( pkt ); //since we are the distinguished learner, write to our log
-			sendChosenToAllLearners( pkt );
-			updateLargestInstanceNum( pkt.getInstanceNumber() );
-		}
-		
-	}
-	
-	private void learnProposal( PaxosPacket pkt ) {
-		Proposal p = new Proposal(pkt);
-		proposals.put(pkt.getInstanceNumber(), p ); //add this proposal to our proposals...
-		executeProposal( p );
-	}
-	
-	private void executeProposal( Proposal p ) {
-		int iNum = p.getInstanceNum();
-		if( iNum == this.lastContInstance + 1 ) {
-			this.lastContInstance++;
-			//ok we can process this
-			learned.put( p.getInstanceNum(), p.getProposalNum() );
-			//now see if we can execute more proposals 
-			this.executeNextLearnedProposal();
-		} else if( iNum > this.lastContInstance + 1 ) {
-			//ok we are missing something so write this proposal to the out of order log
-			writeOutOfOrder( p );
+			Proposal p = new Proposal( pkt );
+			proposalCount.remove(p.getInstanceNum()); //not sure if we should remove from our counts.
+			this.masterLearn( p );
 		}
 	}
 	
-	private void executeNextLearnedProposal() {
-		Proposal p = proposals.get(this.lastContInstance + 1); //the next higher instance
-		executeProposal( p );
+	/**
+	 * Takes a proposal and pushes it to disk as well as tells all the other learners
+	 * @param p
+	 */
+	private void masterLearn( Proposal p ) {
+		executeProposal(p);
+		sendChosenToAllLearners( p );
 	}
 	
-	private void writeToLog( PaxosPacket pkt ) {
-		int instanceNum = pkt.getInstanceNumber();
-		int proposalNum = pkt.getProposalNumber();
-		
-		try {
-			PersistentStorageWriter w = this.n.getWriter(LEARN_FILE, true);
-			w.write(instanceNum + " " + proposalNum );
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	private void writeOutOfOrder( Proposal p ) {
-		try {
-			PersistentStorageWriter w = this.n.getWriter(OUT_OF_ORDER, true);
-			w.write(p.toString());
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	private void sendChosenToAllLearners( PaxosPacket pkt ) {
-		int[] learners = PaxosLayer.LEARNERS;
-		PaxosPacket learnPacket = new PaxosPacket(PaxosProtocol.LEARN, pkt.getProposalNumber(), pkt.getInstanceNumber(), pkt.getPayload());
-		for( int learner : learners ) {
-			this.send(learner, learnPacket);
-		}
+	private void send(int dest, PaxosPacket pkt){
+		this.paxosLayer.send(dest, pkt);
 	}
 	
 	
-	public int learnedProposalNumForInstance( int instanceNum ) {
-		return this.learned.get( instanceNum );
-	}
+	
+	/**
+	 * Methods used by proposer
+	 * 
+	 * 
+	 */
+	
 	
 	public ArrayList<Integer> getMissingInstanceNums() {
 		ArrayList<Integer> missing = new ArrayList<Integer>();
+			try {
+				PersistentStorageReader r = this.n.getReader(LEARN_FILE);
+				String line = r.readLine();
+				while( line != null ) {
+					String[] entry = line.split(" ");
+					int instanceNum = Integer.parseInt(entry[0]);
+					int proposalNum = Integer.parseInt(entry[1]);
+					learned.put( instanceNum, proposalNum );
+					updateLargestInstanceNum( instanceNum );
+				}
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		for( int i = this.lastContInstance + 1; i < this.largestInstanceNum; i++ ) {
 			Proposal p = this.proposals.get(i);
 			if( p == null ) {
@@ -207,6 +200,85 @@ public class LearnerLayer {
 	}
 	
 	public void writeValue( Proposal p ){
-		
+		this.masterLearn(p);
 	}
+	
+	public int learnedProposalNumForInstance( int instanceNum ) {
+		return this.learned.get( instanceNum );
+	}
+	
+	
+	
+	/**
+	 * Disk methods
+	 * 
+	 * 
+	 */
+	
+	
+	private void readLog() {
+		try {
+			PersistentStorageReader r = this.n.getReader(LEARN_FILE);
+			String line = r.readLine();
+			while( line != null ) {
+				String[] entry = line.split(" ");
+				int instanceNum = Integer.parseInt(entry[0]);
+				int proposalNum = Integer.parseInt(entry[1]);
+				learned.put( instanceNum, proposalNum );
+				updateLargestInstanceNum( instanceNum );
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private void writeToLog( Proposal p ) {
+		int instanceNum = p.getInstanceNum();
+		int proposalNum = p.getProposalNum();
+		
+		try {
+			PersistentStorageWriter w = this.n.getWriter(LEARN_FILE, true);
+			w.write(instanceNum + " " + proposalNum );
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void readOutOfOrder() {
+		try {
+			PersistentStorageReader r = this.n.getReader(OUT_OF_ORDER);
+			String line = r.readLine();
+			while( line != null ) {
+				String[] entry = line.split("|");
+				int instanceNum = Integer.parseInt(entry[0]);
+				int proposalNum = Integer.parseInt(entry[1]);
+				String value = entry[2];
+				Proposal p = new Proposal( instanceNum, proposalNum, value );
+				proposals.put( instanceNum, p);
+				updateLargestInstanceNum( instanceNum );
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private void writeOutOfOrder( Proposal p ) {
+		try {
+			PersistentStorageWriter w = this.n.getWriter(OUT_OF_ORDER, true);
+			w.write(p.toString());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	
 }
