e341c2981b8624cbaaefbaa28861c8c574ce89b0
Brad Bicknell <fxfilmxf@gmail.com>
2011-03-07 17:12:28 -0800

    learner comments

--- proj/LearnerLayer.java
+++ proj/LearnerLayer.java
@@ -12,13 +12,18 @@ public class LearnerLayer {
 	private PaxosLayer paxosLayer;
 	private HashMap<Integer, Proposal> proposals; //these are accepted proposals that haven't been processed, instance num, pro num, transaction string
 	private HashMap<Integer, HashMap<Integer, Integer>> proposalCount; //instance num, proposal num, proposal count
-	private HashMap<Integer, Proposal> outOfOrder;
+	private HashMap<Integer, Proposal> outOfOrder; //all out of order proposals
 	
 	private HashMap<Integer, Proposal> learned; //instance num, proposal num
 	private DistNode n;
-	private int lastContInstance;
-	private int largestInstanceNum;
+	private int lastContInstance;  //The largest continuous instance number we have learned
+	private int largestInstanceNum; //
 	
+	/**
+	 * Creates a new learner layer
+	 * @param paxosLayer
+	 * @param n
+	 */
 	public LearnerLayer(PaxosLayer paxosLayer, DistNode n) {
 		this.paxosLayer = paxosLayer;
 		this.n = n;
@@ -31,17 +36,31 @@ public class LearnerLayer {
 		this.learned = new HashMap<Integer, Proposal>();
 	}
 	
+	/**
+	 * Called by the paxos layer. Must be called before the
+	 * proposer layer is started.
+	 */
 	public void start() {
 		this.readLog();
 		this.readOutOfOrder();
 	}
 	
+	/**
+	 * Updates the largest instance number seen
+	 * @param instanceNum
+	 */
 	private void updateLargestInstanceNum( int instanceNum ) {
 		if( instanceNum > this.largestInstanceNum ) {
 			this.largestInstanceNum = instanceNum;
 		}
 	}
 	
+	/**
+	 * Called by the paxos layer when either an accept 
+	 * or learn message is received
+	 * @param from
+	 * @param pkt
+	 */
 	public void receive(int from, PaxosPacket pkt) {
 		if( pkt.getProtocol() == PaxosProtocol.ACCEPT ) {
 			//we are the distinguished learner
@@ -52,12 +71,23 @@ public class LearnerLayer {
 		}
 	}
 	
+	/**
+	 * Called when a learn message is received. Extracts a proposal 
+	 * from the packet.
+	 * @param from
+	 * @param pkt
+	 */
 	private void receivedLearn(int from, PaxosPacket pkt){
 		Proposal p = new Proposal(pkt);
 		executeProposal( p );
 	}
 	
-	
+	/**
+	 * Learns the given proposal if the instance number is one greater
+	 * than the largest continuous instance number we have learned. If not, 
+	 * adds the proposal to the out of order hash and writes the hash to disk
+	 * @param p - given proposal
+	 */
 	private void executeProposal( Proposal p) {
 		int iNum = p.getInstanceNum();
 		
@@ -86,12 +116,21 @@ public class LearnerLayer {
 		}
 	}
 	
+	/**
+	 * Called after a proposal has been learned, and will call execute on and
+	 * proposals that were out of order that can now be executed
+	 */
 	private void executeNextLearnedProposal() {
 		Proposal p = proposals.get(this.lastContInstance + 1); //the next higher instance
 		if( p != null )
 			executeProposal( p);
 	}
 	
+	/**
+	 * Writes the value in the proposal to disk using the transaction layer. Sets the value of the proposal
+	 * to abort if the transaction layer was unable to commit the transaction
+	 * @param p
+	 */
 	private void pushProposalValueToDisk( Proposal p ) {
 		Transaction txn = Transaction.fromString( p.getValue(), this.paxosLayer.getTransactionLayer().cache );
 		boolean committed = commit( txn, txn.willAbort );
@@ -103,6 +142,12 @@ public class LearnerLayer {
 		
 	}
 	
+	/**
+	 * Calls the paxos finished method on the transaction layer with the given transaction
+	 * @param txn
+	 * @param abort
+	 * @return whether or not commit was successful
+	 */
 	private boolean  commit( Transaction txn, boolean abort) {
 		//call the transaction layer to see if we can commit
 		return this.paxosLayer.getTransactionLayer().paxosFinished(txn, abort);
@@ -117,6 +162,10 @@ public class LearnerLayer {
 	 */
 	
 	
+	/**
+	 * Sends the given proposal to all learners in a learn message
+	 * Does not send a message to this current node
+	 */
 	private void sendChosenToAllLearners( Proposal p ) {
 		int[] learners = PaxosLayer.LEARNERS;
 		PaxosPacket learnPacket = new PaxosPacket(PaxosProtocol.LEARN, p.getProposalNum(), p.getInstanceNum(), Utility.stringToByteArray(p.getValue()));
@@ -126,6 +175,13 @@ public class LearnerLayer {
 		}
 	}
 	
+	/**
+	 * Called when an accept message is received. If there is a majority for the instance - proposal number in
+	 * given in the packet, then the learner will learn the proposal, otherwise the method just increments
+	 * the count for the instance number and proposal number.
+	 * @param from
+	 * @param pkt
+	 */
 	private void receivedAccept(int from, PaxosPacket pkt){
 		//current number of the instance/proposal number we have seen so far
 		HashMap<Integer, Integer> instanceHash = proposalCount.get(pkt.getInstanceNumber());
@@ -167,6 +223,11 @@ public class LearnerLayer {
 		sendChosenToAllLearners( p );
 	}
 	
+	/**
+	 * Sends the given packet to the given destination
+	 * @param dest
+	 * @param pkt
+	 */
 	private void send(int dest, PaxosPacket pkt){
 		this.paxosLayer.send(dest, pkt);
 	}
@@ -178,6 +239,11 @@ public class LearnerLayer {
 	 */
 	
 	
+	/**
+	 * Returns the missing instance numbers. These are the instances
+	 * that are missing between the largest continuous and largest instance number
+	 * we have seen.
+	 */
 	public ArrayList<Integer> getMissingInstanceNums() {
 		ArrayList<Integer> missing = new ArrayList<Integer>();
 		for( int i = this.lastContInstance + 1; i < this.largestInstanceNum; i++ ) {
@@ -190,10 +256,18 @@ public class LearnerLayer {
 		return missing;
 	}
 	
+	/**
+	 * Returns the largest instance number we have seen
+	 * @return
+	 */
 	public int getLargestInstanceNum() {
 		return this.largestInstanceNum;
 	}
 	
+	/**
+	 * If the given propsal has not been learned yet, learn the proposal
+	 * @param p
+	 */
 	public void writeValue( Proposal p ){
 		if(!this.learned.containsKey(p.getInstanceNum()))
 			this.masterLearn(p);
@@ -206,7 +280,9 @@ public class LearnerLayer {
 	 * 
 	 */
 	
-	
+	/**
+	 * Reads the learned log and populates the learned hash
+	 */
 	private void readLog() {
 		try {
 			PersistentStorageReader r = this.n.getReader(LEARN_FILE);
@@ -228,7 +304,10 @@ public class LearnerLayer {
 			//e.printStackTrace();
 		}
 	}
-	
+
+	/**
+	 * Writes the learned hash to disk
+	 */
 	private void writeToLearnedLog() {
 		String learnContent = "";
 		for( Integer iNum : this.learned.keySet() ) {
@@ -242,6 +321,9 @@ public class LearnerLayer {
 		}
 	}
 	
+	/**
+	 * Reads the out of order log from disk and populates the hash
+	 */
 	private void readOutOfOrder() {
 		try {
 			PersistentStorageReader r = this.n.getReader(OUT_OF_ORDER);
@@ -264,6 +346,9 @@ public class LearnerLayer {
 		}
 	}
 	
+	/**
+	 * Writes the out of order hash to disk
+	 */
 	private void writeOutOfOrder() {
 		String outoforderContent = "";
 		for( Integer iNum : this.outOfOrder.keySet() ) {
@@ -277,6 +362,12 @@ public class LearnerLayer {
 		}
 	}
 
+	/**
+	 * Returns the learned value for the given instance number. 
+	 * Returns null if there is no learned proposal
+	 * @param instanceNum
+	 * @return
+	 */
 	public String getLearnedForInstance(int instanceNum) {
 		Proposal p = learned.get( instanceNum );
 		if( p != null ) { 
